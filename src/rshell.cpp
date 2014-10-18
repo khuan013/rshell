dc10a41c676e6783b391fcdfeb3f4de84edc7636
Kenneth Huang <khuan013@ucr.edu>
2014-10-18 16:46:28 -0700

    updated rshell.cpp

--- src/rshell.cpp
+++ src/rshell.cpp
@@ -24,6 +24,31 @@ void parse(char* line, vector<string> & input) {
 	}
 }
 
+void execute(vector<string> & input, int start, int end) {
+
+    //Call execvp, based on which elements in the string vector to use
+    char * argv[(end - start)+1];
+	    int i = 0;
+	    
+	    for (i = 0; i <= (end - start); i++) {
+		argv[i] = new char[5];
+	    }
+
+	    for (i = 0; i < (end-start); i++) {
+		strcpy(argv[i], input[i+start].c_str());
+
+	    }
+	    
+	    argv[i] = NULL;
+
+	    
+	    int r = execvp(argv[0], argv);
+	    if (r == -1)
+		perror("execvp");
+		exit(1); 
+	
+}
+
 int main() {
 
     char buffer[512];
@@ -38,41 +63,42 @@ int main() {
 	fgets(buffer, 512, stdin);
 
 	parse(buffer, input);	
-	
 
 	if (strcmp(input[0].c_str(), "exit") == 0)
 		exit(0);
 
-
-	
 	int pid=fork();
-	if (pid ==0) {
+	int pid2;
+	if (pid == 0) {
+	    
 
-	    char * argv[input.size()+1];
-
-	    for (int i = 0; i <= input.size(); i++) {
-		argv[i] = new char[5];
-
-		if (i != input.size())
-		    strcpy(argv[i], input[i].c_str());
+	    int start = 0;
+	    int end = input.size();
+	    int i;
+	    
+	    // Check for semi colons
+	    for (i = 0; i < input.size(); i++) {
+		if (input[i][(input[i].length() -1)] == ';') {
+		    end = i;
+		    pid2 = fork();
+		    break;
+		}
+	    }
+	   
+	    if (pid2==0) {
+		start = i+1;
+		end = input.size();
 	    }
 
-	    argv[input.size()] = NULL;
+	    execute(input, start, end);
 
-
-	    int r = execvp(argv[0], argv);
-	    if (r == -1)
-		perror("execvp");
-		exit(1); 
 	}
-	
-	
 	else {
+	    while(wait(NULL) > 0);
+	}
 
-
-	    wait(NULL);
-	}	
-
+    
+    
     }
     
     return 0; 
