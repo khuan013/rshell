651cad13f9dc89d485d69884c21c6d931bf8dfc1
Kenneth Huang <khuan013@ucr.edu>
2014-10-18 13:53:59 -0700

    added parsing for # comments

--- src/rshell.cpp
+++ src/rshell.cpp
@@ -11,6 +11,19 @@
 
 using namespace std;
 
+
+void parse(char* line, vector<string> & input) {
+    
+    char * pch;
+	pch = strtok (line, " \n");
+	while (pch!= NULL) {
+	    if (*pch == '#')
+		break;
+	    input.push_back(pch);
+	    pch = strtok (NULL, " \n");
+	}
+}
+
 int main() {
 
     char buffer[512];
@@ -23,14 +36,12 @@ int main() {
 		
 	cout << "Please enter cmd: ";
 	fgets(buffer, 512, stdin);
-	
-	char * pch;
-	pch = strtok (buffer, " \n");
-	while (pch!= NULL) {
-	    input.push_back(pch);
-	    pch = strtok (NULL, " \n");
-	}
 
+	parse(buffer, input);	
+	
+
+	if (strcmp(input[0].c_str(), "exit") == 0)
+		exit(0);
 
 
 	
@@ -39,30 +50,28 @@ int main() {
 
 	    char * argv[input.size()+1];
 
-
 	    for (int i = 0; i <= input.size(); i++) {
 		argv[i] = new char[5];
+
 		if (i != input.size())
 		    strcpy(argv[i], input[i].c_str());
 	    }
 
 	    argv[input.size()] = NULL;
 
-	    if (strcmp(argv[0], "exit") == 0) {
-		exit(0);
-	    }
 
 	    int r = execvp(argv[0], argv);
 	    if (r == -1)
 		perror("execvp");
-
-
+		exit(1); 
 	}
+	
+	
 	else {
 
 
 	    wait(NULL);
-	    	}	
+	}	
 
     }
     
